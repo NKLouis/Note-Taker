88c25f75961f1426bb920ca6882cbe9c0d098d4a
naritchaya louis <naritchayalouis@gmail.com>
2020-04-20 14:14:22 -0500

    added  server.js file,routes, html files

---
+++ routes/apiRoute.js
@@ -0,0 +1,59 @@
+const db= require("../db/db.json");
+const fs = require("fs");
+const uuid = require("uuid")
+
+//noteId =[];
+
+module.exports = function(app) {
+   
+    app.get("/api/notes", function(req, res) {
+      res.send(db);
+    });
+
+    app.post("/api/notes", function(req, res) {
+        let noteId = uuid.v4()
+        let newNote = {
+            id: noteId,
+            title: req.body.title,
+            text: req.body.text
+        };
+        
+        fs.readFile("./db/db.json","utf8",(err,data)=> {
+        if (err) throw err
+        
+        const allNotes = JSON.parse(data);
+        allNotes.push(newNote);
+
+        fs.writeFile("./db/db.json",JSON.stringify(allNotes),err => {
+            if (err) throw err
+            res.send(db)
+            console.log("success!!")
+
+
+
+        })
+
+        })
+    });
+    app.delete("/api/notes/:id", function(req, res) {
+        let noteId = req.params.id
+        fs.readFile("./db/db.json","utf8",(err,data)=> {
+            if (err) throw err
+            const allNotes = JSON.parse(data);
+            const updateNote = allNotes.filter(note => note.id!=noteId)
+
+            fs.writeFile("./db/db.json",JSON.stringify(allNotes),err => {
+                if (err) throw err
+                res.send(db)
+                console.log("Note Deleted!!")
+        });
+
+      });
+    
+  
+
+
+
+    });
+
+};  
---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require("express");
+
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+require("./routes/api")(app);
+require("./routes/html")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+
+
+  
